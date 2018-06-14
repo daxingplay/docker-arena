a296d4855a644dcaa2b0918819fd86674193e16f
橘子 <daxingplay@gmail.com>
2018-06-14 19:54:04 +0800

    fix: change config path

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 config_file=/srv/config/index.json
 
 if [ -f $config_file ]; then
-  cp -f $config_file /srv/arena/index.json
+  cp -f $config_file /srv/arena/src/server/config/index.json
 fi
 
 node /srv/arena/index.js
